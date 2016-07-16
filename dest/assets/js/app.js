8a9e4f35173b9e6eeeadef08d3cba6f1091bf0b2
Kyoko Kadowaki <kyoko.kadowaki@gmail.com>
2016-07-17 03:42:17 +0900

    no jquery

--- dest/assets/js/app.js
+++ dest/assets/js/app.js
@@ -49,76 +49,17 @@ app.config(function ($routeProvider) {
     });
 });
 
-app.directive('slideable', function () {
-    return {
-        restrict: 'C',
-        compile: function compile(element, attr) {
-            // wrap tag
-            var contents = element.html();
-            element.html('<div class="slideable_content" style="margin:0 !important; padding:0 !important" >' + contents + '</div>');
-
-            return function postLink(scope, element, attrs) {
-                // default properties
-                attrs.duration = !attrs.duration ? '1s' : attrs.duration;
-                attrs.easing = !attrs.easing ? 'ease-in-out' : attrs.easing;
-                element.css({
-                    'overflow': 'hidden',
-                    'height': '0px',
-                    'transitionProperty': 'height',
-                    'transitionDuration': attrs.duration,
-                    'transitionTimingFunction': attrs.easing
-                });
-            };
-        }
-    };
-}).directive('slideToggle', function () {
-    return {
-        restrict: 'A',
-        link: function link(scope, element, attrs) {
-            var target, content;
-
-            attrs.expanded = false;
-
-            element.bind('click', function () {
-                if (!target) target = document.querySelector(attrs.slideToggle);
-                if (!content) content = target.querySelector('.slideable_content');
-
-                if (!attrs.expanded) {
-                    content.style.border = '1px solid rgba(0,0,0,0)';
-                    var y = content.clientHeight;
-                    content.style.border = 0;
-                    target.style.height = y + 'px';
-                } else {
-                    target.style.height = '0px';
-                }
-                attrs.expanded = !attrs.expanded;
-            });
-        }
-    };
-});
-
 app.controller('contactController', ['$scope', function ($scope) {}]);
 
 app.controller('mainController', ['$scope', function ($scope) {}]);
 
-app.controller('bioController', ['$scope', function ($scope) {
-    $scope.flags = [false, false, false, false];
-    $scope.open = function (i) {
-        console.log(i);
-        $scope.flags[i] = !$scope.flags[i];
-    };
-}]);
+app.controller('bioController', ['$scope', function ($scope) {}]);
 
 app.controller('pjController', ['$scope', function ($scope) {
     $scope.pShow = [true, false];
     $scope.open = function (b) {
-        if (b) {
-            $scope.pShow[0] = true;
-            $scope.pShow[1] = false;
-        } else {
-            $scope.pShow[1] = true;
-            $scope.pShow[0] = false;
-        }
+        $scope.pShow[0] = b;
+        $scope.pShow[1] = !b;
     };
 }]);
 
--- src/js/controllers/controller.js
+++ src/js/controllers/controller.js
@@ -28,56 +28,6 @@ app.config(($routeProvider) =>(
 							)
 )
 
-app.directive('slideable', function () {
-    return {
-        restrict:'C',
-        compile: function (element, attr) {
-            // wrap tag
-            var contents = element.html();
-            element.html('<div class="slideable_content" style="margin:0 !important; padding:0 !important" >' + contents + '</div>');
-
-            return function postLink(scope, element, attrs) {
-                // default properties
-                attrs.duration = (!attrs.duration) ? '1s' : attrs.duration;
-                attrs.easing = (!attrs.easing) ? 'ease-in-out' : attrs.easing;
-                element.css({
-                    'overflow': 'hidden',
-                    'height': '0px',
-                    'transitionProperty': 'height',
-                    'transitionDuration': attrs.duration,
-                    'transitionTimingFunction': attrs.easing
-                });
-            };
-        }
-    };
-})
-.directive('slideToggle', function() {
-    return {
-        restrict: 'A',
-        link: function(scope, element, attrs) {
-            var target, content;
-
-            attrs.expanded = false;
-
-            element.bind('click', function() {
-                if (!target) target = document.querySelector(attrs.slideToggle);
-                if (!content) content = target.querySelector('.slideable_content');
-
-                if(!attrs.expanded) {
-                    content.style.border = '1px solid rgba(0,0,0,0)';
-                    var y = content.clientHeight;
-                    content.style.border = 0;
-                    target.style.height = y + 'px';
-                } else {
-                    target.style.height = '0px';
-                }
-                attrs.expanded = !attrs.expanded;
-            });
-        }
-    }
-});
-
-
 app.controller('contactController', ['$scope',($scope) => {
 }])
 
@@ -85,24 +35,15 @@ app.controller('mainController', ['$scope',($scope) => {
 }])
 
 app.controller('bioController', ['$scope',($scope) => {
-  $scope.flags = [false, false, false, false];
-  $scope.open = function(i){
-    console.log(i);
-    $scope.flags[i] = !$scope.flags[i]
-  };
+
 }])
 
 app.controller('pjController', ['$scope',($scope) => {
   $scope.pShow = [true,false];
   $scope.open = function(b){
-    if(b){
-      $scope.pShow[0] = true;
-      $scope.pShow[1] = false;
-    }else{
-      $scope.pShow[1] = true;
-      $scope.pShow[0] = false;
-    }
-  };
+      $scope.pShow[0] = b;
+      $scope.pShow[1] = !b;
+    };
 }])
 
 app.controller('mapController', ($scope) => {
