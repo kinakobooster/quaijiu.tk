dfbdba32100135d168130d5f6b4e6350f2cfe13b
kinakobooster <afrotoxin.028@gmail.com>
2016-08-17 01:25:18 +0900

    :mewtwo:

--- dest/assets/js/app.js
+++ dest/assets/js/app.js
@@ -2,80 +2,93 @@
 
 function init() {};
 
-var projects = [{ id: "2", title: 'Eternal Unknown Pleasures', detail: 'You can enjoy Unknown Pleasures etarnally...', name: "unkn" }, { id: "1", title: 'YOU ARE DEAD', detail: 'escape from poop death is inevitable', name: "poop" }];
+var projects = [{ id: "2", title: 'Eternal Unknown Pleasures', detail: 'You can enjoy Unknown Pleasures etarnally...', date: '2016/5/5', name: "unkn" }, { id: "1", title: 'YOU ARE DEAD', detail: 'escape from poop death is inevitable', date: '2016/6/5', name: "poop" }];
 
 var app = angular.module('App', ['ui.router']);
 
+var main_content = document.getElementById("main_content");
+
 app.config(function ($stateProvider, $urlRouterProvider) {
-    $urlRouterProvider.otherwise("/");
-    $stateProvider.state('/', {
-        url: '/',
-        templateUrl: 'dest/assets/templates/pages/main.html',
-        controller: function controller($scope) {}
-    }).state('biography', {
-        url: '/biography',
-        templateUrl: 'dest/assets/templates/pages/biography.html',
-        controller: function controller($scope) {}
-    }).state('projects', {
-        url: '/projects',
-        templateUrl: 'dest/assets/templates/pages/projects.html',
-        controller: function controller($scope, $state) {
-            $scope.url = $state;
-            $scope.projects = projects;
-            $scope.pShow = [true, false];
-            $scope.open = function (b) {
-                $scope.pShow[0] = b;
-                $scope.pShow[1] = !b;
-            };
-        }
-    }).state('projects.detail', {
-        url: '/detail/{projectName}',
-        templateUrl: 'dest/assets/templates/pages/projects/pde.html',
-        controller: function controller($stateParams, $scope) {
-            $scope.project = projects.find(function (item) {
-                return item.name === $stateParams.projectName;
-            });
-            $scope.src = 'src/pde/' + $scope.project.name + '.pde';
-            var pde_name = 'pde_script_' + $scope.project.id;
-            var script = document.getElementById(pde_name).innerHTML;
-            var canvas = document.getElementById("processing-canvas");
-            var p = new Processing(canvas, script);
-        }
-    }).state('map', {
-        url: '/map',
-        templateUrl: 'dest/assets/templates/pages/map.html',
-        controller: function controller($scope) {
-            var latlng = new google.maps.LatLng(35.676272, 139.6404296);
-            var myOptions = {
-                zoom: 12,
-                center: latlng,
-                mapTypeId: google.maps.MapTypeId.ROADMAP
-            };
-            var map = new google.maps.Map(document.getElementById('map'), myOptions);
+  $urlRouterProvider.otherwise("/");
+  $stateProvider.state('/', {
+    url: '/',
+    templateUrl: 'dest/assets/templates/pages/main.html',
+    controller: function controller($scope) {
+      main_content.classList.remove('blur');
+    }
+  }).state('biography', {
+    url: '/biography',
+    templateUrl: 'dest/assets/templates/pages/biography.html',
+    controller: function controller($scope) {
+      main_content.classList.add('blur');
+    }
+  }).state('projects', {
+    url: '/projects',
+    templateUrl: 'dest/assets/templates/pages/projects.html',
+    controller: function controller($scope, $state) {
+      main_content.classList.add('blur');
+      $scope.url = $state;
+      $scope.projects = projects;
+      $scope.pShow = [true, false];
+      $scope.open = function (b) {
+        $scope.pShow[0] = b;
+        $scope.pShow[1] = !b;
+      };
+    }
+  }).state('projects.detail', {
+    url: '/detail/{projectName}',
+    templateUrl: 'dest/assets/templates/pages/projects/pde.html',
+    controller: function controller($stateParams, $scope) {
+      main_content.classList.add('blur');
+      $scope.project = projects.find(function (item) {
+        return item.name === $stateParams.projectName;
+      });
+      $scope.src = 'src/pde/' + $scope.project.name + '.pde';
+      var pde_name = 'pde_script_' + $scope.project.id;
+      var script = document.getElementById(pde_name).innerHTML;
+      var canvas = document.getElementById("processing-canvas");
+      var p = new Processing(canvas, script);
+    }
+  }).state('map', {
+    url: '/map',
+    templateUrl: 'dest/assets/templates/pages/map.html',
+    controller: function controller($scope) {
+      main_content.classList.add('blur');
+      var latlng = new google.maps.LatLng(35.676272, 139.6404296);
+      var myOptions = {
+        zoom: 12,
+        center: latlng,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+      };
+      var map = new google.maps.Map(document.getElementById('map'), myOptions);
 
-            var styledMapOptions = { name: 'EIFUKU' };
+      var styledMapOptions = { name: 'EIFUKU' };
 
-            var styleOptions = [{
-                "stylers": [{ "invert_lightness": true }, { "hue": "#003bff" }]
-            }];
+      var styleOptions = [{
+        "stylers": [{ "invert_lightness": true }, { "hue": "#003bff" }]
+      }];
 
-            var markerOptions = {
-                position: latlng,
-                map: map
-            };
-            var marker = new google.maps.Marker(markerOptions);
-            var type = new google.maps.StyledMapType(styleOptions, styledMapOptions);
-            map.mapTypes.set('eifuku', type);
-            map.setMapTypeId('eifuku');
-        }
-    }).state('contact', {
-        url: '/contact',
-        templateUrl: 'dest/assets/templates/pages/contact.html',
-        controller: function controller($scope) {}
-    }).state('poop', {
-        url: 'projects/poop',
-        templateUrl: 'dest/assets/templates/pages/pj_poop.html',
-        controller: function controller($scope) {}
-    });
+      var markerOptions = {
+        position: latlng,
+        map: map
+      };
+      var marker = new google.maps.Marker(markerOptions);
+      var type = new google.maps.StyledMapType(styleOptions, styledMapOptions);
+      map.mapTypes.set('eifuku', type);
+      map.setMapTypeId('eifuku');
+    }
+  }).state('contact', {
+    url: '/contact',
+    templateUrl: 'dest/assets/templates/pages/contact.html',
+    controller: function controller($scope) {
+      main_content.classList.add('blur');
+    }
+  }).state('poop', {
+    url: 'projects/poop',
+    templateUrl: 'dest/assets/templates/pages/pj_poop.html',
+    controller: function controller($scope) {
+      main_content.classList.add('blur');
+    }
+  });
 });
 //# sourceMappingURL=app.js.map
--- src/js/controllers/controller.js
+++ src/js/controllers/controller.js
@@ -1,11 +1,13 @@
 function init(){ };
 
 const projects = [
-  {id: "2" , title: 'Eternal Unknown Pleasures', detail:'You can enjoy Unknown Pleasures etarnally...', name:"unkn"},
-  {id: "1", title: 'YOU ARE DEAD', detail:'escape from poop death is inevitable', name:"poop"}
+  {id: "2" , title: 'Eternal Unknown Pleasures', detail:'You can enjoy Unknown Pleasures etarnally...', date:'2016/5/5',name:"unkn"},
+  {id: "1", title: 'YOU ARE DEAD', detail:'escape from poop death is inevitable', date:'2016/6/5',name:"poop"}
 ];
 
 const app = angular.module('App', ['ui.router']);
+
+var main_content = document.getElementById("main_content");
 
 app.config(function($stateProvider, $urlRouterProvider) {
             $urlRouterProvider.otherwise("/")
@@ -13,17 +15,22 @@ app.config(function($stateProvider, $urlRouterProvider) {
                 .state('/',{
                     url: '/',
                     templateUrl : 'dest/assets/templates/pages/main.html',
-                    controller: ($scope) => { }
+                    controller: ($scope) => {
+                      main_content.classList.remove('blur');
+                    }
                 })
                 .state('biography',{
                     url: '/biography',
                     templateUrl : 'dest/assets/templates/pages/biography.html',
-                    controller: ($scope) => { }
+                    controller: ($scope) => {
+                      main_content.classList.add('blur');
+ }
                 })
                 .state('projects',{
                     url: '/projects',
                     templateUrl : 'dest/assets/templates/pages/projects.html',
                     controller: ($scope, $state) => {
+                      main_content.classList.add('blur');
                       $scope.url = $state;
                       $scope.projects = projects;
                       $scope.pShow = [true,false];
@@ -37,6 +44,7 @@ app.config(function($stateProvider, $urlRouterProvider) {
                     url: '/detail/{projectName}',
                     templateUrl : 'dest/assets/templates/pages/projects/pde.html',
                     controller: ($stateParams,$scope) => {
+                      main_content.classList.add('blur');
                       $scope.project = projects.find((item) => (item.name === $stateParams.projectName));
                       $scope.src = 'src/pde/' + $scope.project.name + '.pde';
                       let pde_name = 'pde_script_' + $scope.project.id;
@@ -49,6 +57,7 @@ app.config(function($stateProvider, $urlRouterProvider) {
                     url: '/map',
                     templateUrl : 'dest/assets/templates/pages/map.html',
                     controller: ($scope) => {
+                      main_content.classList.add('blur');
                       const latlng = new google.maps.LatLng(35.676272,139.6404296);
                       const myOptions = {
                         zoom: 12,
@@ -82,13 +91,14 @@ app.config(function($stateProvider, $urlRouterProvider) {
                     url: '/contact',
                     templateUrl : 'dest/assets/templates/pages/contact.html',
                     controller: ($scope) => {
+                      main_content.classList.add('blur');
                     }
                 })
                 .state('poop',{
                     url: 'projects/poop',
                     templateUrl : 'dest/assets/templates/pages/pj_poop.html',
                     controller: ($scope) => {
-
+                      main_content.classList.add('blur');
                     }
                 });
 							});
